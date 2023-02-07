6c574a34068f5a3a265da42a25dd52b0a040ab26
naimar1996 <naimar@code.edu.az>
2023-02-07 17:08:23 +0400

    Generics

---
+++ ConsoleApp66/GenericList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp66
+{
+    public class GenericList<T>
+    {
+        //public void Generic(T item)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        
+        public string SwapList( T value1, T value2)
+        {
+            T temp;
+           temp = value1;
+            value1 = value2;
+            return $"{value1},{value2}";
+
+        }
+    }
+
+}
---
+++ ConsoleApp66/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp66
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //GenericList <int> intList = new GenericList <int>();
+            //intList.Generic(5);
+            //GenericList <string> strList = new GenericList<string>();
+            //strList.Generic("Hello");
+
+            GenericList<string> strList = new GenericList<string>();
+            Console.WriteLine(strList.SwapList("Salam","Saghol"));
+
+
+
+        }
+
+    }
+
+}
